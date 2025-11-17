a04523d0766ceec04dea39e57a4e8bb1e3e8def6
Han Frisonsova <hanafrisonsova@gmail.com>
2025-11-17 20:33:24 +0100

    add more words and count of mistaces and correct words

--- src/components/Stage/index.tsx
+++ src/components/Stage/index.tsx
@@ -3,30 +3,57 @@ import Wordbox from '../Wordbox';
 import wordList from '../../word-list';
 import './style.css';
 
-// TODO: temporary disable function - remove next line when you start using it
-/* eslint-disable-next-line @typescript-eslint/no-unused-vars */
-const generateWord = (size: number) => {
-  const sizeIndex = size === undefined
-    ? Math.floor(Math.random() * wordList.length)
-    : size - 3;
-  
+const generateWord = (size: number): string => {
+  const sizeIndex =
+    size === undefined ? Math.floor(Math.random() * wordList.length) : size - 3;
+
   if (sizeIndex < 0 || sizeIndex >= wordList.length) {
-    return null;
+    return 'error';
   }
-  
+
   const words = wordList[sizeIndex];
   const wordIndex = Math.floor(Math.random() * words.length);
   return words[wordIndex];
 };
 
 const Stage = () => {
-  const [words] = useState<string[]>(['jahoda']);
+  const [words, setWords] = useState<string[]>([
+    'jahoda',
+    generateWord(6),
+    generateWord(6),
+  ]);
+  const [mistakes, setMistakes] = useState<number>(0);
+  const [correctWords, setCorrectWords] = useState<number>(0);
+
+  const handleMistake = () => {
+    setMistakes((prev) => prev + 1);
+  };
+
+  const handleFinish = () => {
+    setCorrectWords((prev) => prev + 1);
+
+    setWords((prevWords) => {
+      const updated = [...prevWords.slice(1), generateWord(6)];
+      return updated;
+    });
+  };
 
   return (
     <div className="stage">
-      <div className="stage__mistakes">Chyb: 0</div>
+      <div className="stage__mistakes">Chyb: {mistakes}</div>
+      <div className="stage__correct">
+        Správně napsaná slova: {correctWords}
+      </div>
       <div className="stage__words">
-        {words.map((word) => <Wordbox word={word} key={word} />)}
+        {words.map((word, i) => (
+          <Wordbox
+            key={word + i}
+            word={word}
+            active={i === 0}
+            onFinish={handleFinish}
+            onMistake={handleMistake}
+          />
+        ))}
       </div>
     </div>
   );
--- src/components/Wordbox/index.tsx
+++ src/components/Wordbox/index.tsx
@@ -1,15 +1,54 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './style.css';
 
 interface IWordboxProp {
   word: string;
+  onFinish: () => void;
+  active: boolean;
+  onMistake: () => void;
 }
+const Wordbox: React.FC<IWordboxProp> = ({ word, onFinish, active, onMistake }) => {
+  const [lettersLeft, setLettersLeft] = useState<string>(word);
+  const [mistake, setMistake] = useState<boolean>(false);
 
-const Wordbox : React.FC<IWordboxProp> = ({ word }) => {
-  const [lettersLeft] = useState<string>(word);  
-  
+  useEffect(() => {
+    setLettersLeft(word);
+    setMistake(false);
+  }, [word]);
+
+  useEffect(() => {
+    if (!active) return;
+
+    const handleKeyUp = (e: KeyboardEvent) => {
+      const key = e.key.toLowerCase();
+
+       if (!lettersLeft || lettersLeft.length === 0) return;
+
+      if (key === lettersLeft[0]) {
+        setMistake(false);
+
+        if (lettersLeft.length === 1) {
+          onFinish();
+        }
+
+        setLettersLeft((x) => x.slice(1));
+      } else {
+        setMistake(true);
+        onMistake()
+      }
+    };
+
+    document.addEventListener('keyup', handleKeyUp);
+
+    return () => {
+      document.removeEventListener('keyup', handleKeyUp);
+    };
+  }, [lettersLeft, active, onFinish]);
+
   return (
-    <div className="wordbox">{lettersLeft}</div>
+    <div className={mistake ? 'wordbox wordbox--mistake' : 'wordbox'}>
+      {lettersLeft}
+    </div>
   );
 };
 
